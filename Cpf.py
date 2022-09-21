2a3f2dac8828ef7924e9d723660af5030687949b
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-20 22:05:19 -0300

    Implementando método de validação de número de caracteres dentro da classe CPF

--- Cpf.py
+++ Cpf.py
@@ -6,8 +6,17 @@ class Cpf:
         else:
             raise ValueError("CPF inválido!")
 
+    def __str__(self):
+        return self.format_cpf()
     def cpf_valido(self, documento):
         if len(documento) == 11:
             return True
         else:
             return False
+
+    def format_cpf(self):
+        fatia_um = self.cpf[:3]
+        fatia_dois = self.cpf[3:6]
+        fatia_tres = self.cpf[6:9]
+        fatia_quatro = self.cpf[9:]
+        return("{}.{}.{}-{}".format(fatia_um, fatia_dois, fatia_tres, fatia_quatro))
